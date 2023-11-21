11d5c4c94d57ca043e56d37a680046537eca41c5
Victor Ibironke <victoribironke2019@gmail.com>
2023-11-21 13:05:53 +0100

    added seo tags

--- src/components/general/HeadTemplate.tsx
+++ src/components/general/HeadTemplate.tsx
@@ -8,35 +8,35 @@ const HeadTemplate = ({ title }: HeadTemplateProps) => {
       <title>{title ? `${title} — Babcock Tools` : "Babcock Tools"}</title>
       <link rel="shortcut-icon" href={IMAGES.logo.src} type="image/x-icon" />
       <link rel="icon" href={IMAGES.logo.src} type="image/x-icon" />
-      {/* <meta
+      <meta
         name="description"
-        content="List your space, get bookings and get paid from one place."
-      /> */}
+        content="A suite of tools for Babcock University students."
+      />
       {/* <!-- Facebook Meta Tags --> */}
-      {/* <meta property="og:url" content="https://app.ventivo.co" />
+      <meta property="og:url" content="https://www.babcock.tools" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Ventivo" />
+      <meta property="og:title" content="Babcock Tools" />
       <meta
         property="og:description"
-        content="List your space, get bookings and get paid from one place."
+        content="A suite of tools for Babcock University students."
       />
       <meta
         property="og:image"
-        content="https://app.ventivo.co/logo-padding.png"
-      /> */}
+        content="https://www.babcock.tools/images/logo.jpeg"
+      />
       {/* <!-- Twitter Meta Tags --> */}
-      {/* <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="app.ventivo.co" />
-      <meta property="twitter:url" content="https://app.ventivo.co" />
-      <meta name="twitter:title" content="Ventivo" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="www.babcock.tools" />
+      <meta property="twitter:url" content="https://www.babcock.tools" />
+      <meta name="twitter:title" content="Babcock Tools" />
       <meta
         name="twitter:description"
-        content="List your space, get bookings and get paid from one place."
+        content="A suite of tools for Babcock University students."
       />
       <meta
         name="twitter:image"
-        content="https://app.ventivo.co/logo-padding.png"
-      /> */}
+        content="https://www.babcock.tools/images/logo.jpeg"
+      />
     </Head>
   );
 };
