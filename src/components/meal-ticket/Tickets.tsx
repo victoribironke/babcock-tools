8cb43cf0c7c80f8d61d06b38d8e47c8bd79d4417
Victor Ibironke <victoribironke2019@gmail.com>
2024-01-04 20:46:53 +0100

    removed restriction on old tickets

--- src/components/meal-ticket/Tickets.tsx
+++ src/components/meal-ticket/Tickets.tsx
@@ -14,6 +14,13 @@ const Tickets = () => {
   const [noTickets, setNoTickets] = useState(false);
   const [tickets, setTickets] = useState<ATicket[]>([]);
 
+  const [page, setPage] = useState(1);
+
+  const d = tickets.length / 15;
+  const c = Number.isInteger(d)
+    ? page !== Math.floor(d)
+    : page !== Math.floor(d) + 1;
+
   useEffect(() => {
     const q = query(collection(db, "meal-tickets"), where("sold", "==", false));
 
@@ -23,17 +30,18 @@ const Tickets = () => {
       const full_tickets: ATicket[] = [];
 
       querySnapshot.forEach((document) => {
-        if (
-          new Date(document.data().ticket_date).getTime() >
-            new Date().getTime() ||
-          new Date(document.data().ticket_date).toDateString() ===
-            new Date().toDateString()
-        ) {
-          full_tickets.push({
-            id: document.id,
-            ...(document.data() as any),
-          });
-        }
+        // if (
+        //   new Date(document.data().ticket_date).getTime() >
+        //     new Date().getTime() ||
+        //   new Date(document.data().ticket_date).toDateString() ===
+        //     new Date().toDateString()
+        // ) {
+
+        // }
+        full_tickets.push({
+          id: document.id,
+          ...(document.data() as any),
+        });
       });
 
       setLoading(false);
@@ -63,57 +71,90 @@ const Tickets = () => {
   }
 
   return (
-    <section className="w-full flex items-center justify-center flex-wrap mt-10 gap-2">
-      {tickets.map((tk, i) => {
-        const dt = new Date(tk.ticket_date)
-          .toDateString()
-          .split(" ")
-          .slice(0, 3);
-        const date = `${dt[0]}, ${dt[2]} ${dt[1]}`;
+    <>
+      <section className="w-full flex items-center justify-center flex-wrap mt-10 gap-2">
+        {tickets.slice(page * 15 - 15, page * 15).map((tk, i) => {
+          const dt = new Date(tk.ticket_date)
+            .toDateString()
+            .split(" ")
+            .slice(0, 3);
+          const date = `${dt[0]}, ${dt[2]} ${dt[1]}`;
 
-        return (
-          <div
-            key={i}
-            className="bg-white shadow-md border rounded-lg p-2 flex flex-col gap-2"
-          >
-            <div className="flex gap-2">
-              <div className="bg-opacity-10 p-3.5 rounded-md text-4xl grid place-items-center text-yellow bg-yellow">
-                <IoTicketOutline />
-              </div>
-
-              <div className="flex flex-col justify-between">
-                <div className="flex gap-1 text-sm">
-                  <p className="opacity-70">For:</p>
-                  <p>{tk.meal_type}</p>
-                </div>
-
-                <div className="flex gap-1 text-sm">
-                  <p className="opacity-70">On:</p>
-                  <p>{date}</p>
-                </div>
-
-                <div className="flex gap-1 text-sm">
-                  <p className="opacity-70">Price:</p>
-                  <p>₦{formatNumber(parseInt(tk.price))}</p>
-                </div>
-
-                <div className="flex gap-1 text-sm">
-                  <p className="opacity-70">Owner&apos;s hostel:</p>
-                  <p>{tk.hall_of_owner}</p>
-                </div>
-              </div>
-            </div>
-
-            <button
-              className="bg-green text-white rounded-md py-1 px-3"
-              onClick={() => setGetTicketDetails({ ticket_id: tk.id! })}
+          return (
+            <div
+              key={i}
+              className="bg-white shadow-md border rounded-lg p-2 flex flex-col gap-2"
             >
-              Get owner&apos;s details
+              <div className="flex gap-2">
+                <div className="bg-opacity-10 p-3.5 rounded-md text-4xl grid place-items-center text-yellow bg-yellow">
+                  <IoTicketOutline />
+                </div>
+
+                <div className="flex flex-col justify-between">
+                  <div className="flex gap-1 text-sm">
+                    <p className="opacity-70">For:</p>
+                    <p>{tk.meal_type}</p>
+                  </div>
+
+                  <div className="flex gap-1 text-sm">
+                    <p className="opacity-70">On:</p>
+                    <p>{date}</p>
+                  </div>
+
+                  <div className="flex gap-1 text-sm">
+                    <p className="opacity-70">Price:</p>
+                    <p>₦{formatNumber(parseInt(tk.price))}</p>
+                  </div>
+
+                  <div className="flex gap-1 text-sm">
+                    <p className="opacity-70">Owner&apos;s hostel:</p>
+                    <p>{tk.hall_of_owner}</p>
+                  </div>
+                </div>
+              </div>
+
+              <button
+                className="bg-green text-white rounded-md py-1 px-3"
+                onClick={() => setGetTicketDetails({ ticket_id: tk.id! })}
+              >
+                Get owner&apos;s details
+              </button>
+            </div>
+          );
+        })}
+      </section>
+
+      {tickets.length > 0 && (
+        <div className="border mt-4 rounded-lg p-4 flex items-center justify-center gap-2 flex-col rs:flex-row">
+          <p className="text-sm rs:mr-auto">
+            Showing {page * 15 - 14} to{" "}
+            {c
+              ? tickets.length < 15
+                ? tickets.length
+                : page * 15
+              : tickets.length}{" "}
+            of {tickets.length} results
+          </p>
+
+          <div className="flex gap-2">
+            <button
+              className="text-sm border py-1.5 px-3 shadow rounded-md hover:shadow-md disabled:cursor-not-allowed"
+              onClick={() => page !== 1 && setPage((p) => p - 1)}
+              disabled={page === 1}
+            >
+              Previous
+            </button>
+            <button
+              className="text-sm border py-1.5 px-3 shadow rounded-md hover:shadow-md disabled:cursor-not-allowed"
+              onClick={() => c && setPage((p) => p + 1)}
+              disabled={!c}
+            >
+              Next
             </button>
           </div>
-        );
-      })}
-    </section>
+        </div>
+      )}
+    </>
   );
 };
 
