f0418fd5a3022f664d8ccf39e1851f3f52e2c5b0
Victor Ibironke <victoribironke2019@gmail.com>
2024-03-29 12:59:47 +0100

    changed font to bricolage grotesque

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { classNames } from "@/utils/helpers";
 import { Toaster } from "react-hot-toast";
 import localFont from "next/font/local";
+import { Bricolage_Grotesque } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { RecoilRoot } from "recoil";
 import { useRouter } from "next/router";
@@ -10,39 +11,41 @@ import DashboardTemplate from "@/components/dashboard/DashboardTemplate";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorMessage from "@/components/hoc/ErrorMessage";
 
-const gt = localFont({
-  src: [
-    {
-      path: "../../public/fonts/GTWalsheimPro-Black.ttf",
-      weight: "700",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-Bold.ttf",
-      weight: "600",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-Medium.ttf",
-      weight: "500",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-Regular.ttf",
-      weight: "400",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-Light.ttf",
-      weight: "300",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-Thin.ttf",
-      weight: "200",
-    },
-    {
-      path: "../../public/fonts/GTWalsheimPro-UltraLight.ttf",
-      weight: "100",
-    },
-  ],
-  variable: "--font-gt",
-});
+const bg = Bricolage_Grotesque({ subsets: ["latin"] });
+
+// const gt = localFont({
+//   src: [
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Black.ttf",
+//       weight: "700",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Bold.ttf",
+//       weight: "600",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Medium.ttf",
+//       weight: "500",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Regular.ttf",
+//       weight: "400",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Light.ttf",
+//       weight: "300",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-Thin.ttf",
+//       weight: "200",
+//     },
+//     {
+//       path: "../../public/fonts/GTWalsheimPro-UltraLight.ttf",
+//       weight: "100",
+//     },
+//   ],
+//   variable: "--font-gt",
+// });
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
@@ -50,10 +53,10 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <Analytics />
-      <Toaster toastOptions={{ className: gt.className }} />
+      <Toaster toastOptions={{ className: bg.className }} />
       <main
         className={classNames(
-          gt.className,
+          bg.className,
           "min-h-screen w-full flex items-center justify-start flex-col relative",
           router.pathname.includes("dashboard") ? "bg-dark-blue" : "bg-white" //"bg-gradient-to-b from-gray-200 to-[#b1cdfb]"
         )}
