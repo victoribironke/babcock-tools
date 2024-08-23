ba3f72649f6f0139e60205e32f53db63233a2eb6
Victor Ibironke <victoribironke2019@gmail.com>
2024-08-23 17:37:40 +0100

    responsiveness

--- src/pages/dashboard/events.tsx
+++ src/pages/dashboard/events.tsx
@@ -32,7 +32,7 @@ const Events = () => {
     <section className="w-full max-w-[1280px] flex flex-col items-start gap-4">
       <NewEvent />
 
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center justify-center gap-4">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center justify-center gap-4">
         {events.map((e, i) => (
           <EventCard event={e} key={i} />
         ))}
